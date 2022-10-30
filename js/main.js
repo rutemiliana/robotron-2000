eda866140e2b3f67aa21f7d0b8bdea51d542b9cf
Rute Miliana <rutemiliana13@gmail.com>
2022-10-30 12:18:54 -0300

    refatorando e deixando os botões funcionais

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 
-subtrair = document.querySelector("#subtrair")
-somar = document.querySelector("#somar")
-braco = document.querySelector("#braco")
 
 const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach( (elemento) => { 
     elemento.addEventListener("click" , (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent , evento.target.parentNode);
     })
 })
 
-function manipulaDados(operacao){
+function manipulaDados(operacao , controle){
+    peca = controle.querySelector(".controle-contador")
+
     if(operacao == "+"){
-        braco.value = parseInt(braco.value) + 1 ;
+        peca.value = parseInt(peca.value) + 1 ;
 
     } else {
-        braco.value = parseInt(braco.value) - 1 ;
+        peca.value = parseInt(peca.value) - 1 ;
     }
 }
 
